a4ae2897b3b006591a2157007c647d148fe23cf5
Yusuke Suzuki <utatane.tea@gmail.com>
2015-02-24 02:01:07 +0900

    Introduce update tasks into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,10 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var eslint = require('gulp-eslint');
 var istanbul = require('gulp-istanbul');
+var bump = require('gulp-bump');
+var filter = require('gulp-filter');
+var git = require('gulp-git');
+var tagVersion = require('gulp-tag-version');
 
 var SRC = [ 'lib/*.js' ];
 
@@ -77,6 +81,40 @@ gulp.task('lint', function () {
         .pipe(eslint.formatEach('compact', process.stderr))
         .pipe(eslint.failOnError());
 });
+
+
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 -> v0.1.1
+ *     gulp feature   # makes v0.1.1 -> v0.2.0
+ *     gulp release   # makes v0.2.1 -> v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+
+function inc(importance) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json'])
+        // bump the version number in those files
+        .pipe(bump({type: importance}))
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+        // commit the changed version number
+        .pipe(git.commit('Bumps package version'))
+        // read only one file to get the version number
+        .pipe(filter('package.json'))
+        // **tag it in the repository**
+        .pipe(tagVersion());
+}
+
+gulp.task('patch', [ 'travis' ], function () { return inc('patch'); });
+gulp.task('minor', [ 'travis' ], function () { return inc('minor'); });
+gulp.task('major', [ 'travis' ], function () { return inc('major'); });
 
 gulp.task('travis', [ 'lint', 'test' ]);
 gulp.task('default', [ 'travis' ]);
