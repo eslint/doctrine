0155d47af5b38017601712b5f664f8b729f634cd
Nicholas C. Zakas <nicholas@nczconsulting.com>
2015-12-22 09:17:30 -0800

    Build: Update ESLint settings

---
+++ .eslintrc.js
@@ -0,0 +1,209 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "rules": {
+        "accessor-pairs": 2,
+        "array-bracket-spacing": 2,
+        "arrow-body-style": 2,
+        "arrow-parens": 2,
+        "arrow-spacing": 2,
+        "block-scoped-var": 2,
+        "block-spacing": 2,
+        "brace-style": [
+            2,
+            "1tbs"
+        ],
+        "camelcase": 2,
+        "comma-dangle": [
+            2,
+            "never"
+        ],
+        "comma-spacing": [
+            2,
+            {
+                "after": true,
+                "before": false
+            }
+        ],
+        "comma-style": 2,
+        "complexity": 2,
+        "computed-property-spacing": [
+            2,
+            "never"
+        ],
+        "consistent-this": 2,
+        "constructor-super": 2,
+        "curly": 2,
+        "dot-location": 2,
+        "dot-notation": [
+            2,
+            {
+                "allowKeywords": false
+            }
+        ],
+        "eol-last": 2,
+        "generator-star-spacing": 2,
+        "guard-for-in": 2,
+        "handle-callback-err": 2,
+        "id-match": 2,
+        "indent": 2,
+        "jsx-quotes": 2,
+        "key-spacing": 2,
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "max-nested-callbacks": 2,
+        "new-cap": 2,
+        "new-parens": 2,
+        "no-alert": 2,
+        "no-array-constructor": 2,
+        "no-arrow-condition": 2,
+        "no-bitwise": 2,
+        "no-caller": 2,
+        "no-case-declarations": 2,
+        "no-catch-shadow": 2,
+        "no-class-assign": 2,
+        "no-cond-assign": 2,
+        "no-console": 2,
+        "no-const-assign": 2,
+        "no-continue": 2,
+        "no-control-regex": 2,
+        "no-debugger": 2,
+        "no-delete-var": 2,
+        "no-div-regex": 2,
+        "no-dupe-args": 2,
+        "no-dupe-class-members": 2,
+        "no-dupe-keys": 2,
+        "no-duplicate-case": 2,
+        "no-else-return": 2,
+        "no-empty": 2,
+        "no-empty-character-class": 2,
+        "no-empty-label": 2,
+        "no-empty-pattern": 2,
+        "no-eval": 2,
+        "no-ex-assign": 2,
+        "no-extend-native": 2,
+        "no-extra-bind": 2,
+        "no-extra-boolean-cast": 2,
+        "no-extra-parens": [
+            2,
+            "functions"
+        ],
+        "no-extra-semi": 2,
+        "no-fallthrough": 2,
+        "no-floating-decimal": 2,
+        "no-func-assign": 2,
+        "no-implicit-coercion": 2,
+        "no-implied-eval": 2,
+        "no-inner-declarations": [
+            2,
+            "functions"
+        ],
+        "no-invalid-regexp": 2,
+        "no-invalid-this": 2,
+        "no-irregular-whitespace": 2,
+        "no-iterator": 2,
+        "no-label-var": 2,
+        "no-labels": 2,
+        "no-lone-blocks": 2,
+        "no-loop-func": 2,
+        "no-mixed-requires": 2,
+        "no-mixed-spaces-and-tabs": 2,
+        "no-multi-str": 2,
+        "no-multiple-empty-lines": 2,
+        "no-native-reassign": 2,
+        "no-negated-in-lhs": 2,
+        "no-nested-ternary": 2,
+        "no-new": 2,
+        "no-new-func": 2,
+        "no-new-object": 2,
+        "no-new-require": 2,
+        "no-new-wrappers": 2,
+        "no-obj-calls": 2,
+        "no-octal": 2,
+        "no-octal-escape": 2,
+        "no-path-concat": 2,
+        "no-process-env": 2,
+        "no-process-exit": 2,
+        "no-proto": 2,
+        "no-redeclare": 2,
+        "no-regex-spaces": 2,
+        "no-restricted-modules": 2,
+        "no-restricted-syntax": 2,
+        "no-return-assign": 2,
+        "no-script-url": 2,
+        "no-self-compare": 2,
+        "no-sequences": 2,
+        "no-shadow-restricted-names": 2,
+        "no-spaced-func": 2,
+        "no-sparse-arrays": 2,
+        "no-sync": 2,
+        "no-this-before-super": 2,
+        "no-throw-literal": 2,
+        "no-trailing-spaces": 2,
+        "no-undef": 2,
+        "no-undef-init": 2,
+        "no-unexpected-multiline": 2,
+        "no-unneeded-ternary": 2,
+        "no-unreachable": 2,
+        "no-unused-expressions": 2,
+        "no-unused-vars": 2,
+        "no-use-before-define": [
+            2,
+            "nofunc"
+        ],
+        "no-useless-call": 2,
+        "no-useless-concat": 2,
+        "no-void": 2,
+        "no-with": 2,
+        "object-curly-spacing": 2,
+        "operator-assignment": [
+            2,
+            "always"
+        ],
+        "operator-linebreak": 2,
+        "prefer-const": 2,
+        "prefer-spread": 2,
+        "quotes": [
+            2,
+            "single"
+        ],
+        "radix": 2,
+        "require-yield": 2,
+        "semi": [
+            2,
+            "always"
+        ],
+        "semi-spacing": [
+            2,
+            {
+                "after": true,
+                "before": false
+            }
+        ],
+        "space-before-keywords": [
+            2,
+            "always"
+        ],
+        "space-in-parens": 2,
+        "space-infix-ops": 2,
+        "space-return-throw-case": 2,
+        "space-unary-ops": 2,
+        "strict": [
+            2,
+            "function"
+        ],
+        "use-isnan": 2,
+        "valid-jsdoc": 2,
+        "valid-typeof": 2,
+        "wrap-iife": 2,
+        "wrap-regex": 2,
+        "yield-star-spacing": 2,
+        "yoda": [
+            2,
+            "never"
+        ]
+    }
+};
